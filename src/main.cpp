9546fdc720dadc3f4d30f40e5a6acc98ca3e7eec
Fleur De Lotus <bercelou@yahoo.fr>
2024-05-07 13:56:22 +0200

    Setting up the project

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "RPG Game", sf::Style::Default, settings);
+
+    sf::Texture playerTexture;
+    sf::Sprite playerSprite;
+
+    if(playerTexture.loadFromFile("Assets/Player/Textures/spritesheet.png"))
+    {
+        std::cout << "Player images loaded!" << std::endl;
+        playerSprite.setTexture(playerTexture);
+
+        int XIndex = 0;
+        int YIndex = 0;
+
+        playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
+        playerSprite.scale(sf::Vector2f(3, 3));
+    }
+
+    else
+    {
+        std::cout << "Player images failed to loaded!" << std::endl;
+    }
+
+    while(window.isOpen())
+    {
+        sf::Event event; 
+
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if(event.type == sf::Event::KeyPressed)
+            {
+                if(event.key.code == sf::Keyboard::D)
+                {
+                    sf::Vector2f position = playerSprite.getPosition();
+                    playerSprite.setPosition(position + sf::Vector2f(5, 0));
+                }
+            }
+
+            window.clear(sf::Color::Black);
+            window.draw(playerSprite);
+            window.display();
+        }
+    }
+
+    return 0;
+}
